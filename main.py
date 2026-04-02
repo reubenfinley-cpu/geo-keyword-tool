9c61f1122579bbbf5dbb199bd41b984fba02c57a
Peter-Finley <reuben.peter-finley@accenture.com>
2026-04-02 14:45:49 -0400

    add Vercel URL to CORS allow_origins

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*", "https://geo-keyword-tool-psi.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
